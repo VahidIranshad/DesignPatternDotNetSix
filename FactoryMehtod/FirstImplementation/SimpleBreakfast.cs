05c7f47546b6958e3fe1862c122299810126ddd6
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-07 10:05:58 +0330

    add factorymethod

---
+++ FactoryMehtod/FirstImplementation/SimpleBreakfast.cs
@@ -0,0 +1,12 @@
+﻿namespace FactoryMehtod.FirstImplementation
+{
+    internal class SimpleBreakfast : Breakfast
+    {
+        public override void CreateItems()
+        {
+            Items.Add(new Bread());
+            Items.Add(new Cheese());
+            Items.Add(new Tea());
+        }
+    }
+}
