2e89dc74756d660db2d32af4f86e38d3e01d48cf
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-08 10:43:58 +0330

    Add Builder

---
+++ Builder/FirstImplementation/Builder.cs
@@ -0,0 +1,16 @@
+﻿namespace Builder.FirstImplementation
+{
+    internal abstract class Builder
+    {
+        protected LoanRequest loanRequest;
+        public LoanRequest LoanRequest
+        {
+            get { return loanRequest; }
+        }
+
+        // Abstract build methods
+        public abstract void AddPrice();
+        public abstract void AddIntallcount();
+        public abstract void AddRate();
+    }
+}
