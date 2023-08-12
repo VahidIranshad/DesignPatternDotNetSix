ff1a7b98d03211aa7c64779b57a47d67ab238570
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-12 12:42:26 +0330

    Add Composite]

---
+++ Composit/FirstImplementation/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Composit.FirstImplementation
+{
+    internal class MenuItem : Component
+    {
+        public MenuItem(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public string Description { get; set; }
+        public override void Print()
+        {
+            Console.WriteLine($"MenuItem : {Name}");
+        }
+    }
+}
