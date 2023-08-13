3cee93c80a587c73073768a27ea234ff0082b709
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-13 10:58:03 +0330

    add proxy

---
+++ Proxy/FirstImplementation/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Proxy.FirstImplementation
+{
+    internal interface IQuery
+    {
+        void GetData();
+    }
+}
---
+++ Proxy/FirstImplementation/ProxyReqQuery.cs
@@ -0,0 +1,28 @@
+﻿namespace Proxy.FirstImplementation
+{
+    public class ProxyReqQuery : IQuery
+    {
+        ReqQuery _reqQuery;
+        public ProxyReqQuery()
+        {
+            _reqQuery = new ReqQuery();
+        }
+        public void GetData()
+        {
+            if (CheckAccess())
+            {
+                _reqQuery.GetData();
+                Log();
+            }
+        }
+        private bool CheckAccess()
+        {
+            Console.WriteLine("Check Access");
+            return true;
+        }
+        private void Log()
+        {
+            Console.WriteLine("Log");
+        }
+    }
+}
