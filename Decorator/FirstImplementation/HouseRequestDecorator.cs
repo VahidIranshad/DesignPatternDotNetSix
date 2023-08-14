435951948c367d4a5d3692719b95873d138bec4f
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-14 11:47:15 +0330

    add decorator

---
+++ Decorator/FirstImplementation/HouseRequestDecorator.cs
@@ -0,0 +1,14 @@
+﻿namespace Decorator.FirstImplementation
+{
+    internal class HouseRequestDecorator : Decorator
+    {
+        public HouseRequestDecorator(Req req) : base(req)
+        {
+
+        }
+        public override void Display()
+        {
+            Console.WriteLine($"Amount : {this._req.Amount} , InstallCount : {_req.InstallCount}");
+        }
+    }
+}
---
+++ Decorator/FirstImplementation/Req.cs
@@ -0,0 +1,14 @@
+﻿namespace Decorator.FirstImplementation
+{
+    internal abstract class Req
+    {
+        public int Amount { get; set; }
+        public decimal InstallCount { get; set; }
+
+        public virtual void Display()
+        {
+            Console.WriteLine(this.GetType().Name);
+        }
+
+    }
+}
