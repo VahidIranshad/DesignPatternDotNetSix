9aba2dc1d1dc17f70a855d69d2cf5fe7819d8882
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-14 09:27:13 +0330

    Add Bridge

---
+++ Bridge/FirstImplementation/HouseReqValidation.cs
@@ -0,0 +1,10 @@
+﻿namespace Bridge.FirstImplementation
+{
+    internal class HouseReqValidation : IValidation
+    {
+        public void Validate(Req req)
+        {
+            Console.WriteLine("HouseReqValidation");
+        }
+    }
+}
---
+++ Bridge/FirstImplementation/PayByCompany.cs
@@ -0,0 +1,17 @@
+﻿namespace Bridge.FirstImplementation
+{
+    internal class PayByCompany : IPay
+    {
+        private readonly Req _req;
+        public PayByCompany(Req req)
+        {
+            _req = req;
+        }
+        public void Pay(IValidation validation)
+        {
+            Console.WriteLine("Start PayByCompany");
+            validation.Validate(_req);
+            Console.WriteLine("End PayByCompany");
+        }
+    }
+}
