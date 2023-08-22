1facdbafc72461f89b8a8dcf62d93ec3ab932fee
Vahid Iranshad <103821484+VahidIranshad@users.noreply.github.com>
2023-08-22 12:03:16 +0330

    add TemplateMethod

---
+++ TemplateMethod/FirstImplementation/Model.cs
@@ -0,0 +1,8 @@
+﻿namespace TemplateMethod.FirstImplementation
+{
+    internal class Model
+    {
+        public int InstallCount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TemplateMethod.FirstImplementation;
+Model model = new() { InstallCount = 10, Price = 100 };
+Req houseReq = new LoanHouseReq(model);
+Req carReq = new LoanCarReq(model);
+houseReq.Save();
+carReq.Save();
+Console.ReadKey();
